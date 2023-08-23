45cce3b3f8413fb4d8db216cf6e7967618263b9d
Philipp Junk <philipp.junk@ucdconnect.ie>
2023-08-23 19:07:36 +0100

    Cleaned up code

--- src/optimizations/interface.py
+++ src/optimizations/interface.py
@@ -311,7 +311,8 @@ class GeneralConfig(pydantic.BaseModel):
 
     @pydantic.field_validator('objfunc')
     def validate_objfunc(cls, objfunc):
-        valid = ['chi_sq', 'sos', 'sod', 'norm_sos', 'ave_norm_sos']  # TODO add (and test) more
+        valid = ['sos', 'sod']
+        # TODO chi_sq does not work atm? why?
         if objfunc not in valid:
             raise ValueError(f'objfunc must be one of {valid}')
         return objfunc
@@ -321,7 +322,6 @@ class GeneralConfig(pydantic.BaseModel):
 
         # hacked params
         config_dict['models'] = 'np'
-        config_dict['numpy_model'] = True  # TODO reducant with models==np?
         config_dict['_optimization'] = ["_data"]
 
         config_dict['_custom_func'] = func
@@ -384,91 +384,12 @@ def run_simple_optimization(func, inputs, outputs, general_config: GeneralConfig
             raise RuntimeError(f'Unknown fit type: {param_dict["fit_type"]}')
 
     ####################################################################################### 
-    # # IMPORTANT: it is necessary to create pybnf_output/Simulations dir!
-    ###########################################################################
-    # # param_dict = CustomConfiguration.default_config()
-    # param_dict = dict()
-    # # TODO do not start from default_config, instead start from empty dict
-    #
-    # # custom parameters
-    # # TODO document custom parameters in CustomConfiguration!!
-    # param_dict["models"] = "np"
-    # param_dict["numpy_model"] = True
-    # param_dict["_optimization"] = [
-    #     "_data",
-    # ]
-    # param_dict["_custom_data"] = data
-    # param_dict["_custom_func"] = func
-    #
-    # # param_dict["fit_type"] = "de"
-    # # test_param_dict["fit_type"] = "ss"
-    # param_dict["population_size"] = 25
-    # param_dict["max_iterations"] = 30
-    # param_dict["objfunc"] = "sos"
-    #
-    # # TODO set output dir to temporary dir and clean up afterwards
-    #
-    # # TODO investigate how to handle unbound parameters
-    # # TODO consider whether we only want uniform_var parameters
-    # # parameters
-    # for i in range(n_params):
-    #     param_dict[("uniform_var", f"v{i:0{10}d}__FREE")] = [0.0, 10.0, True]
-    # param_dict[("uniform_var", f"v{i:0{10}d}__FREE")] = [-10000000, 10000000, True]
 
-    # for testing, disable dusk parallelization
-    # test_param_dict["parallelize_models"] = 0
+    # TODO set output dir to temporary dir and clean up afterwards
+    #
+    # TODO investigate how to handle unbound parameters
+    # TODO consider whether we only want uniform_var parameters
 
-    ###########################################################################
-
-    # for k, v in config.config.items():
-    # print(k, v)
-
-    # TODO match algorithm type
-    # param_dict["fit_type"] = "de"
-    # pybnf_config = CustomConfiguration(param_dict)
-    # # alg = pybnf.algorithms.DifferentialEvolution(config)
-    #
-    # param_dict["fit_type"] = "ade"
-    # pybnf_config = CustomConfiguration(param_dict)
-    # # alg = pybnf.algorithms.AsynchronousDifferentialEvolution(config)
-    #
-    # param_dict["fit_type"] = "ss"
-    # pybnf_config = CustomConfiguration(param_dict)
-    # # alg = pybnf.algorithms.ScatterSearch(config)
-    #
-    # param_dict["fit_type"] = "pso"
-    # pybnf_config = CustomConfiguration(param_dict)
-    # # alg = pybnf.algorithms.ParticleSwarm(config)
-    #
-    # param_dict["fit_type"] = "mh"
-    # param_dict["max_iterations"] = 300
-    # param_dict["burn_in"] = 100
-    # pybnf_config = CustomConfiguration(param_dict)
-    # # alg = pybnf.algorithms.BasicBayesMCMCAlgorithm(config)
-    # # TODO mh also produces an output file called Results/credible95_final.txt, which I
-    # # assume contains 95% confidence intervals for estimated parameters
-    # # -> add to OptimizeResult output
-    #
-    # param_dict["fit_type"] = "pt"
-    # param_dict["max_iterations"] = 300
-    # param_dict["burn_in"] = 100
-    # pybnf_config = CustomConfiguration(param_dict)
-    # # alg = pybnf.algorithms.BasicBayesMCMCAlgorithm(config)
-    #
-    # param_dict["fit_type"] = "am"
-    # param_dict["max_iterations"] = 500
-    # param_dict["burn_in"] = 100
-    # param_dict["adaptive"] = 100  # max_iterations must be at least 2 x (burn_in + adaptive)
-    # pybnf_config = CustomConfiguration(param_dict)
-    # # alg = pybnf.algorithms.Adaptive_MCMC(config)
-    #
-    # param_dict["fit_type"] = "sa"
-    # param_dict["max_iterations"] = 300
-    # param_dict["burn_in"] = 100
-    # pybnf_config = CustomConfiguration(param_dict)
-    # alg = pybnf.algorithms.BasicBayesMCMCAlgorithm(pybnf_config, sa=True)
-
-    ####################################################################################### 
     # # IMPORTANT: it is necessary to create pybnf_output/Simulations dir!
     # # IMPORTANT: pybnf_output/Results seems also important!
     os.makedirs(os.path.join(pybnf_config.config["output_dir"], "Simulations"), exist_ok=True)
@@ -481,6 +402,11 @@ def run_simple_optimization(func, inputs, outputs, general_config: GeneralConfig
 
     # load results
     results = pd.read_table(os.path.join(pybnf_config.config["output_dir"], "Results", "sorted_params_final.txt"))
+
+    # TODO mh (and possibly other bayesian algorithms as well )
+    # also produces an output file called Results/credible95_final.txt, which I
+    # assume contains 95% confidence intervals for estimated parameters
+    # -> add to OptimizeResult output
 
     # delete dir
     shutil.rmtree(pybnf_config.config['output_dir'])
